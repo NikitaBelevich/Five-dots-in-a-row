ec77528c3f9b19ae808aa099477327e8d8073f51
Nikita Belevich <belevish_1998@mail.ru>
2021-08-11 22:53:49 +0300

    feat: Created a function that generates the game field.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 'use strict';
 
+createGameField();
+function createGameField(rowsCount = 20, colsCount = 20) {
+    const tableField = document.createElement('table');
+    tableField.classList.add('game-field__table');
+    for (let i = 0; i < rowsCount; i++) {
+        const tr = document.createElement('tr');
+        for (let i = 0; i < colsCount; i++) {
+            const td = document.createElement('td');
+            tr.append(td);
+            
+        }
+        tableField.append(tr);
+    }
+    document.querySelector('.game-field').append(tableField);
+    
+}
